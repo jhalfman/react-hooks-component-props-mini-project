a9a957dbcc42cc776b7459bac48ab63f7d1ed2d3
Jordan Halfman <jordanhalfman@gmail.com>
2022-06-28 18:40:23 -0700

    commit tree e90393934a9516e1aae5909781016d5dcc504639

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+    return (
+        <main>
+            {posts.map(post => <Article title={post.title} date={post.date} preview={post.preview} key={post.id}/>)}
+        </main>
+    )
+}
+
+export default ArticleList;
